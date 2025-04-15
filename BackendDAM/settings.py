4a2b64bf53e60c086062a7d6b87bb9c77b22e07c
finsenn <vincentn113@gmail.com>
2025-04-15 10:50:56 +0700

    Update settings.py

--- BackendDAM/settings.py
+++ BackendDAM/settings.py
@@ -74,8 +74,16 @@ WSGI_APPLICATION = 'BackendDAM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': os.getenv("DATABASE_NAME")',
+        'USER' : os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS' : {
+            'driver': 'ODBC Driver 17 for SQL Server'
+        }
+
     }
 }
 
