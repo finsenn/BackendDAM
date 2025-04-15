d84f63ae3706da4253ec5cbc2d6cce565eb74ef6
finsenn <vincentn113@gmail.com>
2025-04-15 16:59:37 +0700

    Update views.py

--- PreprocessData/views.py
+++ PreprocessData/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from utils.log_processor import process_logs
+from .utils.log_processor import process_logs
 from django.http import HttpResponse
 
 def run_log_processing(request):
