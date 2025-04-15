b20e1f2cf300b6fda55772a0a39b1cad22cd8986
finsenn <vincentn113@gmail.com>
2025-04-15 16:54:54 +0700

    a

--- BackendDAM/urls.py
+++ BackendDAM/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include  # ✅ include must be imported
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- PreprocessData/views.py
+++ PreprocessData/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.log_processor import process_logs
+from django.http import HttpResponse
 
 def run_log_processing(request):
     result = process_logs
