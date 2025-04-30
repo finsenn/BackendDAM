35e4bf8a05678e0bd7ae8ad58c06671635e67c52
finsenn <vincentn113@gmail.com>
2025-04-30 14:52:55 +0700

    Update models.py

--- PreprocessData/models.py
+++ PreprocessData/models.py
@@ -24,28 +24,35 @@ class LogEntry(models.Model):
 class TotalQueriesPerDay(models.Model):
     date = models.DateField(unique=True)
     total_queries = models.IntegerField()
+    imported_file = models.ForeignKey(ImportedFile, on_delete=models.CASCADE, null=True, blank=True, related_name='total_queries_per_day_entries')
 
 class TotalQueriesPerUserDay(models.Model):
     date = models.DateField()
     user = models.CharField(max_length=255)
-    total_queries = models.IntegerField()
+    imported_file = models.ForeignKey(ImportedFile, on_delete=models.CASCADE, null=True, blank=True, related_name='total_queries_per_user_day_entries')
 
 class TotalAffectedRows(models.Model):
     date = models.DateField(unique=True)
     total_affected_rows = models.IntegerField()
+    imported_file = models.ForeignKey(ImportedFile, on_delete=models.CASCADE, null=True, blank=True, related_name='total_affected_rows_entries')
+
 
 class AffectedRowsPerUser(models.Model):
     date = models.DateField()
     user = models.CharField(max_length=255)
     total_affected_rows = models.IntegerField()
-
+    imported_file = models.ForeignKey(ImportedFile, on_delete=models.CASCADE, null=True, blank=True, related_name='affected_rows_per_user_entries')
+    
 class SuspiciousQuery(models.Model):
     timestamp = models.DateTimeField()
     date = models.DateField()
     user = models.CharField(max_length=255)
-    query = models.TextField()  
+    query = models.TextField()
+    imported_file = models.ForeignKey(ImportedFile, on_delete=models.CASCADE, null=True, blank=True, related_name='suspicious_queries')  
 
 class HourlyQueryVolume(models.Model):
     date = models.DateField()
     hour = models.IntegerField()
     query_count = models.IntegerField()
+    imported_file = models.ForeignKey(ImportedFile, on_delete=models.CASCADE, null=True, blank=True, related_name='hourly_query_volume_entries')
+    
